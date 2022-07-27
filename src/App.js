af9c5560faf1f320789af53fcc3b1e54322e203c
Leah Sanchez <leahsanchez@Leahs-MacBook-Pro.local>
2022-07-27 12:30:32 -0600

    added post user

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import "./App.css";
 import { Routes, Route } from "react-router-dom";
 import { useState } from "react";
 import HomePage from "./Pages/HomePage";
+import PostUser from "./Pages/PostUser";
 
 const urlEndpoint = process.env.REACT_APP_URL_ENDPOINT;
 
@@ -20,6 +21,19 @@ function App() {
     });
     const responseJSON = await response.json();
     setServerMessage(responseJSON.serverMessage);
+
+    const postUserData = async (userData) => {
+      const url = urlEndpoint + "/create-user";
+      const response = await fetch(url, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(userData),
+      });
+      const responseJSON = await response.json();
+      return responseJSON;
+    };
   };
 
   return (
@@ -36,6 +50,10 @@ function App() {
                 sendReceiveMessage={sendReceiveMessage}
               />
             }
+          />
+          <Route
+            path="/post-user"
+            element={<PostUser postUserData={postUserData} />}
           />
         </Routes>
       </header>
